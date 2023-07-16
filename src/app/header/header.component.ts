4fabf637ae71f50f73a5d5a47fdce8242669ea6c
yasinsiadat <siadatnezhad2@gmail.com>
2023-07-16 20:09:49 +0330

    Account page colors improved

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Router, NavigationEnd , ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,9 @@ import { MenuItem } from 'primeng/api';
 })
 export class HeaderComponent {
   menuItems: MenuItem[];
-
-  constructor() {
+  isAccountPage: boolean = false;
+  
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
     this.menuItems  = [
       {label: 'Home', routerLink: ['/']},
       {label: 'Dashboard',  routerLink: ['/dashboard']},
@@ -19,6 +21,14 @@ export class HeaderComponent {
       {icon:"fa-solid fa-circle-user fa-lg",routerLink: ['/account'] },
       {icon:"fa-solid fa-cart-shopping fa-xl"},
     ];
+  }
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isAccountPage = event.url.includes('/account');
+        console.log("isAccountPage: ",this.isAccountPage )
+      }
+    });
   }
   toggleMenu() {
     // Toggle menu logic here
