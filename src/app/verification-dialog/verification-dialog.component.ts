83b9e841b5b709263dff663de4c050f0491a01a0
yasinsiadat <siadatnezhad2@gmail.com>
2023-07-26 14:58:21 +0330

    Set check verification button and some improvment

--- src/app/verification-dialog/verification-dialog.component.ts
+++ src/app/verification-dialog/verification-dialog.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {DynamicDialogConfig} from "primeng/dynamicdialog";
 import {MetamaskBrightIdService} from 'src/app/metamask-bright-id.service';
 import {MannaService} from 'src/app/manna.service';
+import {UserClaimingState,UserService} from 'src/app/user.service';
+import {MessageService} from 'primeng/api';
 
 
 @Component({
@@ -10,16 +12,49 @@ import {MannaService} from 'src/app/manna.service';
   styleUrls: ['./verification-dialog.component.scss']
 })
 export class VerificationDialogComponent {
-  qrCodeValue: string;
+  qrCodeValue: string = '';
+  isLoading: boolean = false;
+  isLoadingQRCode: boolean = false;
 
   constructor(readonly dynamicDialogConfig: DynamicDialogConfig,
               public metamaskBrightIdService: MetamaskBrightIdService,
-              public mannaService: MannaService) {
-    this.qrCodeValue = `brightid://link-verification/http:%2f%2fnode.brightid.org/idchain/${dynamicDialogConfig.data}`;
+              public mannaService: MannaService,
+              public userService: UserService,
+              private messageService: MessageService) {}
+  ngOnInit() {
+    this.isLoadingQRCode = true;
+    const walletAddress = this.metamaskBrightIdService.account$.getValue();
+    this.metamaskBrightIdService.getVerificationStatus(walletAddress).subscribe(
+      res => {
+        this.qrCodeValue = res.link; 
+        this.isLoadingQRCode = false;
+      },
+      err => {
+        console.error(err);
+        this.isLoadingQRCode = false;
+      }
+    );
   }
 
   onCheckVerify() {
+    this.isLoading = true;
     const walletAddress = this.metamaskBrightIdService.account$.getValue();
-    this.metamaskBrightIdService.checkBrightIdStatus(walletAddress);
+    this.metamaskBrightIdService.getVerificationStatus(walletAddress)
+      .subscribe({
+        next: (response) => {
+          this.isLoading = false;
+          if (response.status === "SUCCESSFUL") {
+            this.userService.userClaimingState$.next(UserClaimingState.VERIFIED);
+            this.messageService.add({severity:'success', summary: 'Success', detail: 'Verification successful!'});
+          } else {
+            this.messageService.add({severity:'error', summary: 'Failed', detail: 'Verification failed. Please try again.'});
+          }
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.messageService.add({severity:'error', summary: 'Error', detail: 'Error during verification.'});
+        },
+      });
   }
+  
 }
