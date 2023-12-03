98abbc2d4223fce3a17cce0593cc47145f64a48a
yasinsiadat <siadatnezhad2@gmail.com>
2023-12-03 17:00:03 +0330

    Store score on locall storage with a week expire

--- src/app/account/user-account/user-account.component.ts
+++ src/app/account/user-account/user-account.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MetamaskBrightIdService } from 'src/app/metamask-bright-id.service';
-import { TuiAlertService, } from '@taiga-ui/core';
+import { TuiAlertService } from '@taiga-ui/core';
 import { Subscription } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -9,7 +9,6 @@ declare let window: any;
 interface ServerResponse {
   data: {
     score: number;
-    // Add other properties as needed
   };
 }
 
@@ -25,6 +24,10 @@ export class UserAccountComponent implements OnInit, OnDestroy {
   accountSubscription: Subscription = new Subscription();
   buttonText = 'Connect';
   loader: boolean = false;
+  userScore: number | null = null; // New property to store the user's score
+  isScoreGreaterThan25: boolean = false; // New property to check if the score is greater than 25
+  private readonly SCORE_STORAGE_KEY = 'userScore';
+  private readonly SCORE_EXPIRATION_KEY = 'scoreExpiration';
 
   constructor(
     readonly alertService: TuiAlertService,
@@ -33,6 +36,11 @@ export class UserAccountComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit() {
+    // Check for stored user score on component initialization
+    this.userScore = this.getUserScoreFromStorage();
+    const storedScore = this.getUserScoreFromStorage();
+    const expirationTimestamp = this.getScoreExpirationFromStorage();
+
     this.accountSubscription = this.metamaskService.account$.subscribe((address) => {
       this.walletAddress = address;
     });
@@ -44,6 +52,13 @@ export class UserAccountComponent implements OnInit, OnDestroy {
     } else {
       console.error('Metamask not detected.');
     }
+    if (storedScore && expirationTimestamp && expirationTimestamp > Date.now()) {
+      // If the stored score is not expired, use it
+      this.userScore = storedScore;
+    } else {
+      // Otherwise, fetch the score from the server
+      this.refreshUserScore();
+    }
   }
 
   ngOnDestroy() {
@@ -52,7 +67,6 @@ export class UserAccountComponent implements OnInit, OnDestroy {
 
   async onConnect() {
     const timeStamp = Math.floor(Number(Date.now().toString()) / 1000);
-   
 
     try {
       const from = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -97,38 +111,73 @@ export class UserAccountComponent implements OnInit, OnDestroy {
   async sendToServer(data: any, url: string, headers: HttpHeaders) {
     try {
       data.user = data.user.toLowerCase();
-  
-  
+
       // Show loader
       // const loader = this.alertService.showLoader('Sending data to the server...');
-  
+
       const response = await this.http.post<ServerResponse>(url, JSON.stringify(data), { headers }).toPromise();
-  
+
       // Close loader on success
       // loader.close();
-  
+
       console.log('Server response:', response);
-  
+
       if (response?.data) {
-        console.log('Score:', response.data.score / 1e6);
-  
+        const score = response.data.score / 1e6;
+        console.log('Score:', score);
+
         // Show success message
         this.alertService.open('Data sent successfully', {
           // status: TuiStatus.Success,
         });
+
+        // Check if the score is greater than 25
+        this.isScoreGreaterThan25 = score > 25;
+
+        // Set user's score
+        this.userScore = score;
+
+        // Store user score in localStorage
+        this.storeUserScoreInStorage(score);
       }
     } catch (error) {
       console.error('Error sending data to server:', error);
-  
+
       // Close loader on error
       // loader.close();
-  
+
       // Show error message
       this.alertService.open('Error sending data to server', {
         // status: TuiStatus.Error,
       });
-  
+
       // Handle specific errors or show user-friendly messages here
+    }
+  }
+
+  private getUserScoreFromStorage(): number | null {
+    return parseFloat(localStorage.getItem(this.SCORE_STORAGE_KEY) || 'null');
+  }
+
+  private getScoreExpirationFromStorage(): number | null {
+    return parseFloat(localStorage.getItem(this.SCORE_EXPIRATION_KEY) || 'null');
+  }
+
+  private storeUserScoreInStorage(score: number): void {
+    localStorage.setItem(this.SCORE_STORAGE_KEY, score.toString());
+
+    // Set expiration time to one week from now
+    const expirationTimestamp = Date.now() + 7 * 24 * 60 * 60 * 1000;
+    localStorage.setItem(this.SCORE_EXPIRATION_KEY, expirationTimestamp.toString());
+  }
+
+  private async refreshUserScore(): Promise<void> {
+    // Fetch the score from the server and handle the response
+    try {
+      // ... (fetching score from the server logic)
+    } catch (error) {
+      // Handle error fetching score
+      console.error('Error fetching score from server:', error);
     }
   }
 }
