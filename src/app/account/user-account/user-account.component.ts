22e3a0546ba0879a276cf3207cfee34f934991e0
yasinsiadat <siadatnezhad2@gmail.com>
2023-12-05 18:35:06 +0330

    Fix the bug dont show the score  metamask not conn

--- src/app/account/user-account/user-account.component.ts
+++ src/app/account/user-account/user-account.component.ts
@@ -4,7 +4,8 @@ import { TuiAlertService } from '@taiga-ui/core';
 import { Subscription } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { UserScoreService } from '../../user-score.service';
-import {serverUrl} from '../../config';
+import { serverUrl } from '../../config';
+import { MetamaskState } from '../../metamask-bright-id.service';
 
 declare let window: any;
 
@@ -20,14 +21,14 @@ interface ServerResponse {
   styleUrls: ['./user-account.component.scss']
 })
 export class UserAccountComponent implements OnInit, OnDestroy {
-  cards: Card[] = [];
   items!: string[];
   walletAddress: string | null = null;
   accountSubscription: Subscription = new Subscription();
   buttonText = 'Connect';
   loader: boolean = false;
-  userScore: number | null = null; // New property to store the user's score
-  isScoreGreaterThan25: boolean = false; // New property to check if the score is greater than 25
+  userScore: number | null = null;
+  isScoreGreaterThan25: boolean = false;
+  showScore: boolean = false; // Added property to control score visibility
   private readonly SCORE_STORAGE_KEY = 'userScore';
   private readonly SCORE_EXPIRATION_KEY = 'scoreExpiration';
 
@@ -37,7 +38,7 @@ export class UserAccountComponent implements OnInit, OnDestroy {
     readonly alertService: TuiAlertService,
     private metamaskService: MetamaskBrightIdService,
     private http: HttpClient,
-    private userScoreService: UserScoreService, 
+    private userScoreService: UserScoreService,
   ) {}
 
   ngOnInit() {
@@ -45,69 +46,85 @@ export class UserAccountComponent implements OnInit, OnDestroy {
     this.userScore = this.getUserScoreFromStorage();
     const storedScore = this.getUserScoreFromStorage();
     const expirationTimestamp = this.getScoreExpirationFromStorage();
-    if (this.userScore !== null && this.userScore >= 0) {
-      this.userScoreService.userScore = this.userScore;
-    }
-    this.accountSubscription = this.metamaskService.account$.subscribe((address) => {
-      this.walletAddress = address;
-    });
 
-    if (window.ethereum) {
-      window.ethereum.on('accountsChanged', (accounts: string[]) => {
-        this.updateButtonText(accounts);
-      });
-    } else {
-      console.error('Metamask not detected.');
-    }
-    if (storedScore && expirationTimestamp && expirationTimestamp > Date.now()) {
-      // If the stored score is not expired, use it
-      this.userScore = storedScore;
-    } else {
-      // Otherwise, fetch the score from the server
-      this.refreshUserScore();
-    }
-    if (this.userScore !== null && this.userScore >= 0) {
-      this.userScoreAvailable.emit(true); // Notify the parent that the user has a score
-    } else {
-      this.userScoreAvailable.emit(false); // Notify the parent that the user doesn't have a score
-    }
+    // Check Metamask state before subscribing to the account$
+    this.metamaskService.checkMetamaskState().subscribe((metamaskState) => {
+      switch (metamaskState) {
+        case MetamaskState.CONNECTED:
+          this.accountSubscription = this.metamaskService.account$.subscribe((address) => {
+            this.walletAddress = address;
+          });
+
+          if (window.ethereum) {
+            window.ethereum.on('accountsChanged', (accounts: string[]) => {
+              this.updateButtonText(accounts);
+            });
+          } else {
+            console.error('Metamask not detected.');
+          }
+
+          // Continue with the existing logic to check for stored score and fetch if needed
+          if (storedScore && expirationTimestamp && expirationTimestamp > Date.now()) {
+            this.userScore = storedScore;
+          } else {
+            this.refreshUserScore();
+          }
+
+          if (this.userScore !== null && this.userScore >= 0) {
+            this.showScore = true;
+            this.userScoreAvailable.emit(true); // Notify the parent that the user has a score
+          } else {
+            this.showScore = false;
+            this.userScoreAvailable.emit(false); // Notify the parent that the user doesn't have a score
+          }
+          break;
+
+        case MetamaskState.NOT_CONNECTED:
+          // Metamask is not connected, hide the score
+          this.showScore = false;
+          this.buttonText = 'Connect';
+          break;
+
+        case MetamaskState.READY:
+          // Metamask is ready, but we want to hide the score
+          this.showScore = false;
+          this.userScoreAvailable.emit(false);
+          break;
+
+        default:
+          // Handle other Metamask states if needed
+          break;
+      }
+    });
   }
 
   ngOnDestroy() {
     this.accountSubscription.unsubscribe();
   }
 
-  async onConnect() {
+  async onSign() {
     const timeStamp = Math.floor(Number(Date.now().toString()) / 1000);
 
     try {
-      const from = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const msg = `Verification request\naddress: ${from[0]}\ntimestamp: ${timeStamp}`;
-      const sign = await window.ethereum.request({
-        method: 'personal_sign',
-        params: [msg, from[0], timeStamp],
+      const msg = `Verification request\naddress: ${this.walletAddress}\ntimestamp: ${timeStamp}`;
+
+      // Use the new signMessage method
+      this.metamaskService.signMessage(msg).subscribe((sign: string) => {
+        console.log('Signature:', sign);
+        console.log('Timestamp:', timeStamp);
+        this.buttonText = 'Check Score';
+        const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+        this.loader = true;
+        this.sendToServer(
+          {
+            signature: sign,
+            user: this.walletAddress,
+            timestamp: timeStamp,
+          },
+          'signing/gitcoinPassportScore',
+          headers
+        );
       });
-
-      console.log('Signature:', sign);
-      console.log('Timestamp:', timeStamp);
-
-      this.buttonText = 'Check Score';
-
-      // Set Content-Type header
-      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-
-      // Show loader before sending data
-      this.loader = true;
-
-      await this.sendToServer(
-        {
-          signature: sign,
-          user: from[0],
-          timestamp: timeStamp,
-        },
-        'signing/gitcoinPassportScore',
-        headers
-      );
     } catch (error) {
       console.error('Error connecting or sending data to server:', error);
     } finally {
@@ -115,6 +132,8 @@ export class UserAccountComponent implements OnInit, OnDestroy {
       this.loader = false;
     }
   }
+
+
   private updateButtonText(accounts: string[]) {
     this.buttonText = accounts.length > 0 ? 'Check Score' : 'Connect';
   }
@@ -124,47 +143,24 @@ export class UserAccountComponent implements OnInit, OnDestroy {
     try {
       data.user = data.user.toLowerCase();
 
-      // Show loader
-      // const loader = this.alertService.showLoader('Sending data to the server...');
-
       const response = await this.http.post<ServerResponse>(url, JSON.stringify(data), { headers }).toPromise();
-
-      // Close loader on success
-      // loader.close();
-
       console.log('Server response:', response);
 
       if (response?.data) {
         const score = response.data.score / 1e6;
         console.log('Score:', score);
-
-        // Show success message
         this.alertService.open('Data sent successfully', {
-          // status: TuiStatus.Success,
         });
-
-        // Check if the score is greater than 25
         this.isScoreGreaterThan25 = score > 25;
-
-        // Set user's score
         this.userScore = score;
-
-        // Store user score in localStorage
         this.storeUserScoreInStorage(score);
         this.userScoreService.userScore = score;
+        this.showScore = true;
       }
     } catch (error) {
       console.error('Error sending data to server:', error);
-
-      // Close loader on error
-      // loader.close();
-
-      // Show error message
       this.alertService.open('Error sending data to server', {
-        // status: TuiStatus.Error,
       });
-
-      // Handle specific errors or show user-friendly messages here
     }
   }
 
@@ -178,25 +174,14 @@ export class UserAccountComponent implements OnInit, OnDestroy {
 
   private storeUserScoreInStorage(score: number): void {
     localStorage.setItem(this.SCORE_STORAGE_KEY, score.toString());
-
-    // Set expiration time to one week from now
     const expirationTimestamp = Date.now() + 7 * 24 * 60 * 60 * 1000;
     localStorage.setItem(this.SCORE_EXPIRATION_KEY, expirationTimestamp.toString());
   }
 
   private async refreshUserScore(): Promise<void> {
-    // Fetch the score from the server and handle the response
     try {
-      // ... (fetching score from the server logic)
     } catch (error) {
-      // Handle error fetching score
       console.error('Error fetching score from server:', error);
     }
   }
-}
-
-class Card {
-  name?: string;
-  image?: string;
-  price?: number;
 }
--- src/app/metamask-bright-id.service.ts
+++ src/app/metamask-bright-id.service.ts
@@ -1,148 +1,139 @@
-import {Inject, Injectable, Injector} from '@angular/core';
-import {ethers, providers} from 'ethers';
-import {BehaviorSubject, catchError, firstValueFrom, from, map, Observable, of, switchMap} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {MannaService} from "./manna.service";
-import {UserService} from "./user.service";
-import {TuiAlertService, TuiDialogService} from "@taiga-ui/core";
-import {chainConfig, mannaChainId, serverUrl} from "./config";
-import {userVerificationStatus} from 'brightid_sdk_v6';
-
+import { Injectable, Injector } from '@angular/core';
+import { ethers } from 'ethers';
+import { BehaviorSubject, from, Observable, of, catchError, tap, map } from 'rxjs';
+import { TuiAlertService, TuiDialogService } from '@taiga-ui/core';
+import { chainConfig, mannaChainId } from './config';
+import { userVerificationStatus } from 'brightid_sdk_v6';
 
 declare let ethereum: any;
 
 declare global {
-    interface Window {
-        ethereum: any;
-    }
+  interface Window {
+    ethereum: any;
+  }
 }
 
 export enum MetamaskState {
-    NOT_INSTALLED = "NOT_INSTALLED",
-    NOT_CONNECTED = "NOT_CONNECTED",
-    CONNECTED = "CONNECTED",
-    WRONG_CHAIN = "WRONG_CHAIN",
-    READY = "READY",
+  NOT_INSTALLED = 'NOT_INSTALLED',
+  NOT_CONNECTED = 'NOT_CONNECTED',
+  CONNECTED = 'CONNECTED',
+  WRONG_CHAIN = 'WRONG_CHAIN',
+  READY = 'READY',
 }
 
 export enum BrightIdState {
-    NOT_VERIFIED = "NOT_VERIFIED",
-    VERIFIED = "VERIFIED",
-    UNIQUE_VERIFIED = "UNIQUE_VERIFIED",
+  NOT_VERIFIED = 'NOT_VERIFIED',
+  VERIFIED = 'VERIFIED',
+  UNIQUE_VERIFIED = 'UNIQUE_VERIFIED',
 }
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class MetamaskBrightIdService {
-    network$ = new BehaviorSubject<ethers.providers.Network | null>(null);
-    account$ = new BehaviorSubject<string>('');
-    balance$ = new BehaviorSubject<ethers.BigNumber | null>(null);
-    qrcodeValue: string = '';
-    public brightIdVerifiedData: any = null;
+  network$ = new BehaviorSubject<ethers.providers.Network | null>(null);
+  account$ = new BehaviorSubject<string>('');
+  balance$ = new BehaviorSubject<ethers.BigNumber | null>(null);
+  qrcodeValue: string = '';
+  public brightIdVerifiedData: any = null;
 
-    constructor(
-        private http: HttpClient,
-        readonly alertService: TuiAlertService,
-        readonly dialogService: TuiDialogService,
-        readonly mannaService: MannaService,
-        readonly userService: UserService,
-        readonly injector: Injector,
-    ) {
-    }
+  constructor(
+    private readonly alertService: TuiAlertService,
+    private readonly dialogService: TuiDialogService,
+    private readonly injector: Injector,
+  ) {}
 
-    connect(): Observable<string> {
-        return from((window as any).ethereum.request({method: 'eth_requestAccounts'}))
-            .pipe(
-                map((accounts: any) => accounts[0])
-            );
-    }
+  connect(): Observable<string> {
+    return from((window as any).ethereum.request({ method: 'eth_requestAccounts' })).pipe(
+      map((accounts: any) => accounts[0]),
+    );
+  }
 
-    public checkMetamaskState(): Observable<MetamaskState> {
-        return new Observable(subscriber => {
-            if (typeof ethereum == 'undefined') {
-                subscriber.next(MetamaskState.NOT_INSTALLED);
-                return;
-            }
-            from(ethereum.request({method: 'eth_accounts'}) as Promise<any[]>)
-                .subscribe({
-                    next: (account: any[]) => {
-                        if (account.length == 0) {
-                            subscriber.next(MetamaskState.NOT_CONNECTED);
-                            return;
-                        }
-                        this.account$.next(account[0]);
-                        subscriber.next(MetamaskState.CONNECTED);
-                        const provider = new ethers.providers.Web3Provider(ethereum);
-                        from(provider.getNetwork())
-                            .subscribe({
-                                next: network => {
-                                    if (network.chainId == mannaChainId) {
-                                        subscriber.next(MetamaskState.READY);
-                                    } else {
-                                        subscriber.next(MetamaskState.WRONG_CHAIN)
-                                    }
-                                },
-                                error: err => {
-                                    this.alertService.open("Failed to load web3 network", {
-                                        status: "error"
-                                    }).subscribe();
-                                }
-                            })
-                    },
-                    error: err => {
-                        this.alertService.open("Failed to load web3 accounts", {
-                            status: "error"
-                        }).subscribe();
-                    }
-                })
-        })
-    }
+  checkMetamaskState(): Observable<MetamaskState> {
+    return new Observable((subscriber) => {
+      if (typeof ethereum == 'undefined') {
+        subscriber.next(MetamaskState.NOT_INSTALLED);
+        return;
+      }
+      from(ethereum.request({ method: 'eth_accounts' }) as Promise<any[]>).subscribe({
+        next: (account: any[]) => {
+          if (account.length == 0) {
+            subscriber.next(MetamaskState.NOT_CONNECTED);
+            return;
+          }
+          this.account$.next(account[0]);
+          subscriber.next(MetamaskState.CONNECTED);
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          from(provider.getNetwork()).subscribe({
+            next: (network) => {
+              if (network.chainId == mannaChainId) {
+                subscriber.next(MetamaskState.READY);
+              } else {
+                subscriber.next(MetamaskState.WRONG_CHAIN);
+              }
+            },
+            error: (err) => {
+              this.alertService.open('Failed to load web3 network', {
+                status: 'error',
+              });
+            },
+          });
+        },
+        error: (err) => {
+          this.alertService.open('Failed to load web3 accounts', {
+            status: 'error',
+          });
+        },
+      });
+    });
+  }
 
-    public checkBrightIdState(): Observable<BrightIdState> {
-        return from(userVerificationStatus("Manna", `${this.account$.value}`, {
-            signed: "eth",
-            timestamp: "milliseconds"
-        }))
-            .pipe(
-                tap((data: any) => {
-                    if (data.verified) {
-                        this.brightIdVerifiedData = data;
-                    }
-                }),
-                map((data: any) => {
-                    if (data.verified && data.unique) {
-                        return BrightIdState.UNIQUE_VERIFIED;
-                    } else if (data.verified) {
-                        return BrightIdState.VERIFIED;
-                    } else {
-                        return BrightIdState.NOT_VERIFIED;
-                    }
-                }),
-                catchError((error: any) => {
-                    this.qrcodeValue = `brightid://link-verification/Manna/${this.account$.value}`;
-                    this.alertService.open("User not found. Please verify using the QR code.", {
-                        status: "error"
-                    }).subscribe();
-                    return of(BrightIdState.NOT_VERIFIED);
-                })
-            );
-    }
-
-    switchToMannaChain(): Observable<any> {
-        return from(ethereum.request(chainConfig));
-    }
-
-
-    async loadBalance() {
-        if (this.account$.value) {
-            const provider = new ethers.providers.Web3Provider(ethereum);
-            const signer = provider.getSigner();
-            const balance = await signer.getBalance();
-            this.balance$.next(balance);
-        } else {
-            this.balance$.next(null);
+  checkBrightIdState(): Observable<BrightIdState> {
+    return from(
+      userVerificationStatus('Manna', `${this.account$.value}`, {
+        signed: 'eth',
+        timestamp: 'milliseconds',
+      }),
+    ).pipe(
+      tap((data: any) => {
+        if (data.verified) {
+          this.brightIdVerifiedData = data;
         }
+      }),
+      map((data: any) => {
+        if (data.verified && data.unique) {
+          return BrightIdState.UNIQUE_VERIFIED;
+        } else if (data.verified) {
+          return BrightIdState.VERIFIED;
+        } else {
+          return BrightIdState.NOT_VERIFIED;
+        }
+      }),
+      catchError((error: any) => {
+        this.qrcodeValue = `brightid://link-verification/Manna/${this.account$.value}`;
+        this.alertService.open('User not found. Please verify using the QR code.', {
+          status: 'error',
+        });
+        return of(BrightIdState.NOT_VERIFIED);
+      }),
+    );
+  }
+
+  switchToMannaChain(): Observable<any> {
+    return from(ethereum.request(chainConfig));
+  }
+
+  async loadBalance() {
+    if (this.account$.value) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const balance = await signer.getBalance();
+      this.balance$.next(balance);
+    } else {
+      this.balance$.next(null);
     }
+  }
+  signMessage(message: string): Observable<string>  {
+    return from((window as any).ethereum.request({ method: 'personal_sign', params: [message, this.account$.value] }) as Promise<string>);
+  }
 }
