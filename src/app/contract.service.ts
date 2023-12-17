054ae8b936adee32a3c4e72fbfb652df118802f1
yasinsiadat <siadatnezhad2@gmail.com>
2023-12-17 23:52:13 +0330

    Fix the bug for submitting score to contract

--- src/app/contract.service.ts
+++ src/app/contract.service.ts
@@ -1,99 +1,79 @@
 import { Injectable } from '@angular/core';
-import { ethers, JsonRpcProvider, Fragment, JsonFragment } from 'ethers';
-import { from, Observable,throwError } from 'rxjs';
-import { chainConfig, mannaContractAddress, mannaContractABI, claimMannaContractAddress, claimMannaContractABI } from './config';
+import { ethers, Contract } from 'ethers';
+import { from, Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { MetamaskBrightIdService } from './metamask-bright-id.service';
 import { TuiAlertService } from '@taiga-ui/core';
-import { tap, catchError } from 'rxjs/operators';
+import { mannaContractAddress, mannaContractABI, claimMannaContractAddress, claimMannaContractABI } from './config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContractService {
-  private provider: JsonRpcProvider;
-  private mannaContract: ethers.Contract;
-  private claimMannaContract: ethers.Contract;
+  private mannaContract?: Contract;
+  private claimMannaContract?: Contract;
 
   constructor(
     private metamaskService: MetamaskBrightIdService,
     private alertService: TuiAlertService
-    ) {
-    this.provider = new ethers.JsonRpcProvider(chainConfig.params[0].rpcUrls[0]);
-    this.mannaContract = new ethers.Contract(
-      mannaContractAddress,
-      mannaContractABI as Array<string | Fragment | JsonFragment>,
-      this.provider
-    );
-
-    this.claimMannaContract = new ethers.Contract(
-      claimMannaContractAddress,
-      claimMannaContractABI as Array<string | Fragment | JsonFragment>,
-      this.provider
-    );
+  ) {
     this.initializeContracts();
   }
 
-  private initializeContracts(): void {
-    this.metamaskService.account$.subscribe((walletAddress) => {
+  private async initializeContracts(): Promise<void> {
+    this.metamaskService.account$.subscribe(async (walletAddress) => {
       if (walletAddress) {
-        this.provider.getSigner(walletAddress).then(signer => {
-          this.mannaContract = new ethers.Contract(
-              mannaContractAddress,
-              mannaContractABI,
-              signer
-          );
+        const provider = new ethers.BrowserProvider(window.ethereum);
+        const signer = await provider.getSigner();
 
-          this.claimMannaContract = new ethers.Contract(
-              claimMannaContractAddress,
-              claimMannaContractABI,
-              signer
-          );
-        });
+        this.mannaContract = new Contract(
+          mannaContractAddress,
+          mannaContractABI,
+          signer
+        );
+
+        this.claimMannaContract = new Contract(
+          claimMannaContractAddress,
+          claimMannaContractABI,
+          signer
+        );
       }
     });
   }
 
   balanceOf(walletAddress: string): Observable<string> {
-    return from(this.mannaContract['balanceOf'](walletAddress).then(balance => ethers.formatEther(balance)));
+    return from(this.mannaContract!['balanceOf'](walletAddress).then(balance => ethers.formatEther(balance)));
   }
 
   getUserScore(userAddress: string): Observable<{timestamp: number, score: number}> {
-    return from(this.claimMannaContract['userScores'](userAddress).then(response => {
+    return from(this.claimMannaContract!['userScores'](userAddress).then(response => {
         const timestamp = parseInt(response[0].toString());
         const score = parseInt(response[1].toString());
         return { timestamp, score };
     }));
-}
+  }
+
   getScoreThreshold(): Observable<number> {
-    return from(this.claimMannaContract['scoreThreshold']().then(threshold => parseInt(threshold.toString())));
+    return from(this.claimMannaContract!['scoreThreshold']().then(threshold => parseInt(threshold.toString())));
   }
 
   claimManna(): Observable<void> {
-    return from(this.claimMannaContract['claim']().then(claimTx => claimTx.wait()));
+    return from(this.claimMannaContract!['claim']().then(claimTx => claimTx.wait()));
   }
 
-  submitScore(score: number, userAddress: string): Observable<void> {
-    return from(this.claimMannaContract['submitScore'](score, userAddress).then(submitScoreTx => submitScoreTx.wait()));
-  }
   submitUserScore(address: string, scoreData: any): Observable<void> {
     if (!scoreData.signature) {
       console.error('No signature in score data');
       return throwError('No signature in score data');
     }
-    console.log('Data sent for score submission:', {
-      score: scoreData.score, 
-      address, 
-      v: scoreData.signature.v, // Keep 'v' as it is, assuming it's already a string or number
-      r: scoreData.signature.r.startsWith('0x') ? scoreData.signature.r : '0x' + scoreData.signature.r, 
-      s: scoreData.signature.s.startsWith('0x') ? scoreData.signature.s : '0x' + scoreData.signature.s
-    });
 
-    return from(this.claimMannaContract['submitScore'](
+    return from(this.claimMannaContract!['submitScore'](
       scoreData.score, 
-      address,  
-      scoreData.signature.v, // Keep 'v' as it is
-      scoreData.signature.r.startsWith('0x') ? scoreData.signature.r : '0x' + scoreData.signature.r, 
-      scoreData.signature.s.startsWith('0x') ? scoreData.signature.s : '0x' + scoreData.signature.s
+      [scoreData.timestamp, 
+        scoreData.signature.v, 
+        scoreData.signature.r, 
+        scoreData.signature.s]
+      
     ).then(tx => tx.wait())).pipe(
       tap(() => this.alertService.open('Score submitted successfully.', { status: 'success', label: 'Success' }).subscribe()),
       catchError(error => {
@@ -103,7 +83,4 @@ export class ContractService {
       })
     );
   }
-
-
 }
-
