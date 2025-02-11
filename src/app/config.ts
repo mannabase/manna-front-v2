a3dbadaa14ebd6bbbeda2ff6896b98fcf77d6828
dexter <thedexterdev@gmail.com>
2025-02-11 05:28:36 +0100

    Change base url to mn.zxcode.xyz

--- src/app/config.ts
+++ src/app/config.ts
@@ -819,6 +819,7 @@ export let claimMannaContractABI = [
     },
 ];
 
+// export let serverUrl = 'https://mannatest.hedgeforhumanity.org/backend';
 export let serverUrl = 'https://mannatest.hedgeforhumanity.org/backend';
 // export let serverUrl = 'http://backend:7231';
 
